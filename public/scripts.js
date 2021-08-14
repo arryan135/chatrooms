29504012b75af7683bdbb0a2de2d3e2595955a8f
arryan <arryan@umich.edu>
2021-08-13 23:10:34 -0400

    add event listener to namespace images

--- public/scripts.js
+++ public/scripts.js
@@ -14,8 +14,18 @@ socket.on("nsData", nsData => {
   let namespacesDiv = document.querySelector(".namespaces");
   namespacesDiv.innerHTML = "";
   nsData.forEach(ns => {
-    namespacesDiv.innerHTML += `<div class = "namespace"><img src = "${ns.img}"/></div>`
-  })
+    namespacesDiv.innerHTML += `<div class = "namespace" ns=${ns.endpoint}><img src = "${ns.img}"/></div>`
+  });
+
+  // add a click listener for each namespace
+  const arrayRep = Array.from(document.getElementsByClassName("namespace"));
+
+  arrayRep.forEach(element => {
+    element.addEventListener("click", event => {
+      const endPoint = element.getAttribute("ns");
+      console.log(`${endPoint} I should go to now`);
+    });
+  });
 })
 
 socket.on("joined", msg => console.log(msg));
