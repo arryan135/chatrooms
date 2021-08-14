ab7f5e3e3117c54d207dddae7364bcfc978366ff
arryan <arryan@umich.edu>
2021-08-14 19:26:00 -0400

    add search box functionality

--- public/joinRoom.js
+++ public/joinRoom.js
@@ -25,4 +25,19 @@ function joinRoom(roomName){
     document.querySelector(".curr-room-num-users").innerHTML = `${numMembers} <span class="glyphicon glyphicon-user"></span>`;
     document.querySelector(".curr-room-text").innerText = `${roomName}`;
   });
+
+
+  let searchBox = document.querySelector("#search-box");
+  searchBox.addEventListener("input", event => {
+    let messages = Array.from(document.getElementsByClassName("message-text"));
+
+    messages.forEach(msg => {
+      if (msg.innerText.toLowerCase().indexOf(event.target.value.toLowerCase()) === -1){
+        // the message does not contain the search term
+        msg.style.display = "none";
+      } else{
+        msg.style.display = "block";
+      }
+    });
+  });
 }
